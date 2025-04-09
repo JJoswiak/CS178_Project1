f76155fd6fd04f970f68edd41291fb05661b3455
Joseph Joswiak <josephjoswiak@Josephs-MacBook-Pro.local>
2025-04-09 17:40:33 -0500

    v5

--- flaskapp.py
+++ flaskapp.py
@@ -1,26 +1,23 @@
-from flask import Flask, render_template
-import pymysql
-import creds  # Don't forget to import creds.py
+from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-def get_connection():
-    return pymysql.connect(
-        host=creds.host,
-        user=creds.user,
-        password=creds.password,
-        database=creds.db,
-        cursorclass=pymysql.cursors.DictCursor
-    )
+# MySQL configuration
+app.config['MYSQL_HOST'] = 'database-1.czqkw0ewck2n.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'BucksInSix'
+app.config['MYSQL_DB'] = 'movies'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    connection = get_connection()
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT title, year FROM movies LIMIT 10;")
-            movies = cursor.fetchall()
-    return render_template('index.html', movies=movies)
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM movies LIMIT 5')  # Modify query as needed
+    data = cur.fetchall()
+    cur.close()
+    return str(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
